d2cbb07b611ab118fd0eab4550b9ae461c241ec9
ykyoon <ykyoon@robotis.com>
2021-08-12 14:53:41 +0900

    first commit

---
+++ src/topic_sub.cpp
@@ -0,0 +1,32 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+public:
+  MinimalSubscriber()
+  : Node("minimal_subscriber")
+  {
+    const auto QOS_PROFILE = rclcpp::QoS(rclcpp::KeepAll()).reliable().transient_local();
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", QOS_PROFILE, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "get: '%s'", msg->data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
